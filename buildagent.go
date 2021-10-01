4e4796c82233df581748444a307e4232812ec9a7
nikx <87845569+nikx-gupta@users.noreply.github.com>
2021-10-01 15:48:07 +0530

    update agent

--- buildagent.go
+++ buildagent.go
@@ -3,8 +3,11 @@ package buildagent
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func Run() error {
@@ -17,40 +20,54 @@ func Run() error {
 			return
 		}
 
-		fmt.Print(json)
+		go HandlePushEvent(&json)
 		c.Done()
 	})
 
 	r.POST("/loginJSON", func(c *gin.Context) {
-		var json Login
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+		// var json Login
+		// if err := c.ShouldBindJSON(&json); err != nil {
+		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		// 	return
+		// }
 
-		fmt.Print(json)
+		// if json.User != "manu" || json.Password != "123" {
+		// 	c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		// 	return
+		// }
 
-		if json.User != "manu" || json.Password != "123" {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+		// c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 
 	return r.Run()
 }
 
+func HandlePushEvent(event *GitEvent) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("No Home Directory: %s \n", err.Error())
+		return
+	}
+
+	fmt.Printf("Temp Dir: %s \n", homeDir)
+	cloneDir := homeDir + "/tmp/" + event.Repository.Name + "-" + uuid.NewString()
+	fmt.Printf("Clone Dir: %s \n", cloneDir)
+
+	cmd := exec.Command("git", "clone", event.Repository.Url, cloneDir)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error in Executing Git Clone: %s \n", err.Error())
+	}
+
+	cmd.Wait()
+	fmt.Println("Git clone completed")
+}
+
 type GitRepository struct {
-	Url string `json:"url"`
+	Url  string `json:"url"`
+	Name string `json:"name"`
 }
 
 type GitEvent struct {
-	Repository GitRepository `json:"repository"`
-}
-
-type Login struct {
-	User       string        `form:"user" json:"user" xml:"user"  binding:"required"`
-	Password   string        `form:"password" json:"password" xml:"password" binding:"required"`
 	Repository GitRepository `json:"repository"`
 }
